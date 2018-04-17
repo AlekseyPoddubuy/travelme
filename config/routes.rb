bb60b000d0d790662bd91769dabd93de6805a5b4
Aleksey Poddubnuy <aprduction@gmail.com>
2018-04-17 11:49:25 +0300

    Add franchise page and link at logo

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 	root to: 'pages#index'
 
 	get 'index' => 'pages#index', as: :index
+	get 'franchise' => 'pages#franchise', as: :franchise
 	get 'index2' => 'pages#index2', as: :index2
 	get 'admin' => 'pages#admin', as: :admin
 
