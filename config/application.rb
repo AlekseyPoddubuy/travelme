c874a8b9dc1de080869a1cf31d1410e21ad4a1a4
Aleksey Poddubnuy <aprduction@gmail.com>
2018-04-25 17:39:43 +0300

    Add ua l default

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Travel
     config.load_defaults 5.1
 
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :ua
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
