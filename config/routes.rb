27f5c7a6d7b1578074cc2162697c338c6dd6cfdb
Aleksey Poddubnuy <aprduction@gmail.com>
2018-05-05 11:50:21 +0300

    Add phones

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
+	devise_for :admins
 #	resources :metatags
-#	devise_for :admins
 #	resources :photos
 #	resources :tours
 	root to: 'pages#closed'
 
-	#scope ":locale" do
-get 'closed' => 'pages#closed', as: :closed
-	#	get 'index' => 'pages#index', as: :index
-	#	get 'franchise' => 'pages#franchise', as: :franchise
-	#	get 'index2' => 'pages#index2', as: :index2
-	#	get 'admin' => 'pages#admin', as: :admin
-#	end
+	scope ":locale" do
+		get 'closed' => 'pages#closed', as: :closed
+#		get 'index' => 'pages#index', as: :index
+#		get 'franchise' => 'pages#franchise', as: :franchise
+#		get 'index2' => 'pages#index2', as: :index2
+#		get 'admin' => 'pages#admin', as: :admin
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
