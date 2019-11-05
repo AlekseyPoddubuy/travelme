a24870cc599507bfb0cb56fd7557cec189369dbc
Aleksey <aprduction@gmail.com>
2019-11-05 15:05:17 +0200

    add city to form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < MailForm::Base
 	attribute :name, :validate => true
 	attribute :phone, :validate => true
-	attribute :mail, :validate => true
+	attribute :city, :validate => true
+	attribute :mail
 	attribute :text, :validate => true
 
 	def headers 
